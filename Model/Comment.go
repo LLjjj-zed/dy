e3af8d7a2f4be7509ad2ad1566cf8793cc8c2a78
wagaaa <yui12349856@live.com>
2023-02-08 16:45:02 +0800

    comment action updated

--- Model/Comment.go
+++ Model/Comment.go
@@ -2,13 +2,25 @@ package Model
 
 import (
 	user "douyin.core/handler/User"
+	"douyin.core/handler/interaction"
 	"gorm.io/gorm"
 )
 
 type Comment struct {
 	gorm.Model
-	CommentID   uint      `json:"commentID" gorm:"primary key;auto increment"`
-	VideoId     Video     `json:"videoId" gorm:""`
-	UserId      user.User `json:"userId" gorm:"foreignKey:ID"`
-	CommentText string    `json:"commentText" gorm:"varchar(40);not null"`
+	CommentID  uint      `json:"commentID" gorm:"primary key;auto increment"`
+	VideoId    Video     `json:"videoId" gorm:""`
+	UserId     user.User `json:"userId" gorm:"foreignKey:ID"`
+	Content    string    `json:"content" gorm:"varchar(40);not null"`
+	CreateDate string    `json:"create_date"`
+}
+
+func NewComment(Req *interaction.CommentRequest) error {
+	return nil
+}
+
+func DeleteComment(commentID string) error {
+	var comment Comment
+	err := DB.Where("CommentID = ?", commentID).Delete(&comment).Error
+	return err
 }

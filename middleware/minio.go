6642cfbb0554ebf43742354d19ab9870b43aadf9
lljjj <2312680664@qq.com>
2023-02-15 14:45:25 +0800

    增加中间件minio

--- middleware/minio.go
+++ middleware/minio.go
@@ -9,13 +9,14 @@ import (
 
 var bucketName = "douyin"
 
-func Initminio() *minio.Client {
-	endpoint := "play.min.io"
-	accessKeyID := "Q3AM3UQ867SPQQA43P2F"
-	secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+func Initminio() (*minio.Client, context.Context) {
+	endpoint := "http://23.94.57.209:9001"
+	accessKeyID := "IlOzGpW6BAYRuKGO"
+	secretAccessKey := "IzLLsT2WsN8xY8SymTtLu6pzMJDwNxpI"
 	useSSL := true
 
 	// Initialize minio client object.
+	ctx := context.Background()
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
 		Secure: useSSL,
@@ -25,15 +26,18 @@ func Initminio() *minio.Client {
 	}
 
 	log.Printf("%#v\n", minioClient) // minioClient is now set up
-	return minioClient
+	policy := "{\"Version\": \"2012-10-17\",\"Statement\": [{\"Effect\": \"Allow\",\"Action\": [\"s3:GetBucketLocation\"],\"Resource\": [\"arn:aws:s3:::starcity\"]},{\"Effect\": \"Allow\",\"Action\": [\"s3:ListBucket\"],\"Resource\": [\"arn:aws:s3:::starcity\"],\"Condition\": {\"StringEquals\": {\"s3:prefix\": [\"bear\",\"prefix/\"]}}},{\"Effect\": \"Allow\",\"Action\": [\"s3:GetObject\"],\"Resource\": [\"arn:aws:s3:::starcity/bear*\",\"arn:aws:s3:::starcity/prefix/*\"]}]}"
+	minioClient.SetBucketPolicy(ctx, "douyin", policy)
+
+	return minioClient, ctx
 }
 
-func UploadFileToMinio(minioClient *minio.Client, videoname, videopath string) {
+func UploadFileToMinio(minioClient *minio.Client, videoname, videopath string, ctx context.Context) {
 	// Upload the mp4 file
-	ctx := context.Background()
+
 	objectName := videoname
 	filePath := videopath
-	contentType := "application/mp4"
+	contentType := "video/mp4"
 
 	// Upload the mp4 file with FPutObject
 	info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})

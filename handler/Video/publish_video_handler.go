73de56595f910da6531b401adf4d675457ebb1dc
lljjj <2312680664@qq.com>
2023-02-16 22:43:12 +0800

    完善minio，ffmpeg相关代码，重写ffmpeg

--- handler/Video/publish_video_handler.go
+++ handler/Video/publish_video_handler.go
@@ -4,11 +4,24 @@ import (
 	user "douyin.core/handler/User"
 	"douyin.core/middleware"
 	"github.com/gin-gonic/gin"
+	"github.com/minio/minio-go/v7"
 	"net/http"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+var (
+	minioClient    *minio.Client
+	NewminioClient sync.Once
+)
+
+func GetminioClient() {
+	NewminioClient.Do(func() {
+		minioClient = middleware.Initminio()
+	})
+}
 
 // PublishVideoResponse 用户投稿回复结构体
 type PublishVideoResponse struct {
@@ -22,32 +35,32 @@ func PublishVedioHandler(c *gin.Context) {
 	title := c.PostForm("title")
 	token, ok := c.GetQuery("token")
 	if !ok {
-		PublishvideoErr(c, "未能成功获取token，请重试")
+		PublishVideoErr(c, "未能成功获取token，请重试")
 		return
 	}
 	//从token中解析出用户id
 	userclaim, err := middleware.JwtParseUser(token)
 	if err != nil {
-		PublishvideoErr(c, err.Error())
+		PublishVideoErr(c, err.Error())
 	}
 	userid := userclaim.Userid
 	//从请求中获取时视频数据
 	file, err := c.FormFile("data")
 	if err != nil {
-		PublishvideoErr(c, err.Error())
+		PublishVideoErr(c, err.Error())
 		return
 	}
 	//从视频数据中获取视频后缀格式
 	ext := filepath.Ext(file.Filename)
 	if ext != ".mp4" {
-		PublishvideoErr(c, "上传视频格式错误，请重试")
+		PublishVideoErr(c, "上传视频格式错误，请重试")
 		return
 	}
 	videoDao := NewVideoDao()
 	//获取用户视频序号，用于生成视频文件名
 	codeint, err := videoDao.GetUserVideoCode(userid)
 	if err != nil {
-		PublishvideoErr(c, err.Error())
+		PublishVideoErr(c, err.Error())
 		return
 	}
 	//将用户视频序号转换成字符串
@@ -57,27 +70,46 @@ func PublishVedioHandler(c *gin.Context) {
 	username, err := userinfo.GetUserNameByUserID(userid)
 	name := username
 	if err != nil {
-		PublishvideoErr(c, err.Error())
+		PublishVideoErr(c, err.Error())
 	}
 	//将视频持久化到本地，使用strings.Builder替换+提高性能
+	videoname := GetFilename(name, code, ext)
+	path := filepath.Join("./puclic", videoname)
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		PublishVideoErr(c, err.Error())
+		return
+	}
+	imagename := GetFilename(name, code, ".jpg")
+	//生成截图
+	err = middleware.GetSnapshotCmd(videoname, imagename)
+	if err != nil {
+		PublishVideoErr(c, err.Error())
+		return
+	}
+	//连接minio
+	GetminioClient()
+	//将视频上传至minio
+	err = middleware.UploadVideoToMinio(c, minioClient, videoname, path, "video")
+	if err != nil {
+		PublishVideoErr(c, err.Error())
+	}
+	//将视频信息持久化到数据库
+	err = videoDao.PersistNewVideo(title, userid, codeint, videoname, imagename, &userinfo)
+	if err != nil {
+		PublishVideoErr(c, err.Error())
+	}
+	PublishVideoOk(c)
+}
+
+func GetFilename(name, code, ext string) string {
 	var build strings.Builder
 	build.WriteString(name)
-	build.WriteString("code")
+	build.WriteString("-code-")
 	build.WriteString(code)
 	build.WriteString(ext)
 	filename := build.String()
-	path := filepath.Join("../puclic", filename)
-	err = c.SaveUploadedFile(file, path)
-	if err != nil {
-		PublishvideoErr(c, err.Error())
-		return
-	}
-	//将视频信息持久化到数据库
-	err = videoDao.PersistNewVideo(title, userid, &userinfo)
-	if err != nil {
-		PublishvideoErr(c, err.Error())
-	}
-	PublishVideoOk(c)
+	return filename
 }
 
 // PublishVideoOk 返回正确信息
@@ -88,7 +120,7 @@ func PublishVideoOk(c *gin.Context) {
 }
 
 // PublishvideoErr 返回错误信息
-func PublishvideoErr(c *gin.Context, errmeassage string) {
+func PublishVideoErr(c *gin.Context, errmeassage string) {
 	c.JSON(http.StatusOK, &PublishVideoResponse{
 		StatusCode: 1,
 		StatusMsg:  errmeassage,
